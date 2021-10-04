b043c3e4136a169ea8d07339eb5612c35d5808e6
Jaakko Virtanen <jaakko.j.virtanen@tuni.fi>
2021-10-04 20:12:31 +0300

    Add and Delete functions complete

--- src/components/SideBarData.js
+++ src/components/SideBarData.js
@@ -4,7 +4,7 @@ import * as FaIcons from 'react-icons/fa';
 export const SideBarData = [
 	{
 		title: 'Home',
-		path: '/',
+		path: '/home',
 		icon: <FaIcons.FaHome />,
 		cName: 'nav-text'
 	},
@@ -12,6 +12,12 @@ export const SideBarData = [
 		title: 'Info',
 		path: '/info',
 		icon: <FaIcons.FaInfoCircle />,
+		cName: 'nav-text'
+	},
+	{
+		title: 'Dev',
+		path: '/dev',
+		icon: <FaIcons.FaAnchor />,
 		cName: 'nav-text'
 	},
 ]
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,41 @@
 import React from 'react'
+import './home.css';
+
 
 function Home() {
+	const [todo, setTodo] = React.useState([])
+	const [current, setCurrent] = React.useState('')
+
+	function handleSubmit(e) {
+		e.preventDefault() //Prevents refresh on submission
+
+		const newTodo = {
+			id: new Date().getTime(),
+			text: current,
+			completed: false,
+		}
+
+		setTodo([...todo].concat(newTodo)) //Adds new todo to Todos Array
+		setCurrent('') //Reinitialize current to make sure no mix ups
+	}
+
+	function deleteItem(id){
+		const updated = [...todo].filter((current) => current.id !== id)
+		setTodo(updated)
+	}
+
 	return (
-		<div>
-			
-		</div>
+		<div className="home">
+			<form onSubmit={handleSubmit}>
+				<input type="text" onChange={(e) => setCurrent(e.target.value)} value={current} />
+				<button type="submit">Add Item</button>
+			</form>
+
+			{todo.map((item) => <div className="list" key={item.id} >
+				<div>{item.text}</div>
+				<button onClick={() => deleteItem(item.id)}>Delete</button>
+			</div>)}
+		</div> 
 	)
 }
 
